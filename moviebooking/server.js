0a54dbc77b93fca12997bca7be6fd5257750dba6
Anuj Sharma <anujankur13@gmail.com>
2022-03-14 17:43:58 +0530

    Checkpoint5

--- moviebooking/server.js
+++ moviebooking/server.js
@@ -17,4 +17,19 @@ const app = http.createServer((req,res)=>{
     }
 })
 
+const db = require("./app/models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+    
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
 app.listen(port);
